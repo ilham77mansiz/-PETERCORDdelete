39415aef9dad650a997bfcc5899e0ef2b12ef795
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2021-09-06 00:00:56 +0700

    Create sudo.py

---
+++ userbot/plugins/sudo.py
@@ -0,0 +1,7 @@
+SUDO_COMMAND_HAND_LER
+import heroku3
+from telethon.tl.functions.users import GetFullUserRequest
+
+Heroku = heroku3.from_key(HEROKU_API_KEY)
+heroku_api = "https://api.heroku.com"
+sudousers = SUDO_USERS
